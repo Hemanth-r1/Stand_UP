c6402fd82a31c68dfaa1793e8cfa1dcd1d0975d0
hemanth-r1 <hemanth.r8892@gmail.com>
2021-04-06 23:27:27 +0530

    Initial Commit

--- app/src/main/java/in/hr/standup/MainActivity.java
+++ app/src/main/java/in/hr/standup/MainActivity.java
@@ -3,6 +3,7 @@ package in.hr.standup;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
 
+import android.app.AlarmManager;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
@@ -11,6 +12,7 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.widget.CompoundButton;
 import android.widget.Toast;
 import android.widget.ToggleButton;
@@ -20,6 +22,8 @@ public class MainActivity extends AppCompatActivity {
     private static final int NOTIFICATION_ID =0;
     private static final String PRIMARY_CHANNEL_ID = "primary_notification_channel";
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +31,18 @@ public class MainActivity extends AppCompatActivity {
         ToggleButton alarmToggle = findViewById(R.id.alarmToggle);
 
         mNotificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+        Intent notifyIntent = new Intent(this, AlarmReceiver.class);
+        PendingIntent notifyPendingIntent = PendingIntent.getBroadcast
+                (this, NOTIFICATION_ID, notifyIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+
+        boolean alarmUp = (PendingIntent.getBroadcast(this, NOTIFICATION_ID, notifyIntent,
+                PendingIntent.FLAG_NO_CREATE) != null);
+        alarmToggle.setChecked(alarmUp);
+
+
+        AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
 
         alarmToggle.setOnCheckedChangeListener(
                 new CompoundButton.OnCheckedChangeListener() {
@@ -45,8 +61,39 @@ public class MainActivity extends AppCompatActivity {
                         //Show a toast to say the alarm is turned on or off.
                         Toast.makeText(MainActivity.this, toastMessage, Toast.LENGTH_SHORT)
                                 .show();
+
+                        if (isChecked){
+                            /*deliverNotification(MainActivity.this);
+                            //Set the toast message for the ON case
+                            toastMessage = "Stand UP alarm ON!";
+
+                             */
+                            long repeatInterval = AlarmManager.INTERVAL_FIFTEEN_MINUTES;
+                            long triggerTime = SystemClock.elapsedRealtime() + repeatInterval;
+
+                            //If the Toggle is turned on, set the repeating alarm with a 15 min interval
+
+                            if (alarmManager != null){
+                                alarmManager.setInexactRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP, triggerTime
+                                , repeatInterval, notifyPendingIntent);
+                            }
+
+
+                        }else {
+                            /*//Cancel notification if the alarm is turned OFF case
+                            mNotificationManager.cancelAll();
+
+                            //Set the toast message for the off case
+                            toastMessage ="Stand UP alarm OFF!";
+
+                             */
+                            if (alarmManager != null){
+                                alarmManager.cancel(notifyPendingIntent);
+                            }
+                        }
                     }
                 });
+
     }
     /**
      * Creates a Notification channel, for OREO and higher.
@@ -59,7 +106,6 @@ public class MainActivity extends AppCompatActivity {
         // So, add a check on SDK version.
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
-
             //Create a Notification Channel with all the parameters
             NotificationChannel notificationChannel = new NotificationChannel(PRIMARY_CHANNEL_ID,
                     "Stand UP Notification", NotificationManager.IMPORTANCE_HIGH);
@@ -70,7 +116,6 @@ public class MainActivity extends AppCompatActivity {
             notificationChannel.setDescription("Notifies every 15 min to stand up and walk");
             mNotificationManager.createNotificationChannel(notificationChannel);
         }
-
     }
     private void deliverNotification(Context context) {
         Intent contentIntent = new Intent(context, MainActivity.class);
@@ -87,6 +132,6 @@ public class MainActivity extends AppCompatActivity {
                 .setAutoCancel(true)
                 .setDefaults(NotificationCompat.DEFAULT_ALL);
 
+        mNotificationManager.notify(NOTIFICATION_ID, builder.build());
     }
-
 }
